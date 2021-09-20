ed7313552ba5fcb79f2716565c43d7279f67e743
Martin Wernstål <m4rw3r@gmail.com>
2021-09-20 02:18:25 +0200

    test: more tests

--- tests/ExtenderTest.php
+++ tests/ExtenderTest.php
@@ -4,20 +4,91 @@ declare(strict_types=1);
 
 namespace GraphQLASTExtender;
 
-use GraphQL\Language\Parser;
 use GraphQL\Language\AST\DocumentNode;
 use GraphQL\Language\AST\NodeList;
+use GraphQL\Language\Parser;
+use GraphQL\Language\Printer;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class ExtenderText extends TestCase {
-    public function testNone(): void {
-        $document = Parser::parse("type Query { foo: String }");
-        $emptyDocument = new DocumentNode([
+    public function testEmpty(): void {
+        $base = new DocumentNode([
+            "definitions" => new NodeList([]),
+        ]);
+        $extension = new DocumentNode([
             "definitions" => new NodeList([]),
         ]);
 
-        $newDocument = Extender::extend($document, $emptyDocument);
+        $extended = Extender::extend($base, $extension);
 
-        $this->assertSame($newDocument, $document);
+        $this->assertSame($base, $extended);
+    }
+
+    public function testEmptyExtension(): void {
+        $base = Parser::parse("type Query { foo: String }");
+        $extension = new DocumentNode([
+            "definitions" => new NodeList([]),
+        ]);
+
+        $extended = Extender::extend($base, $extension);
+
+        $this->assertSame($base, $extended);
+        $this->assertSame("type Query {
+  foo: String
+}
+", Printer::doPrint($extended));
+    }
+
+    public function testEmptyBase(): void {
+        $base = new DocumentNode([
+            "definitions" => new NodeList([]),
+        ]);
+        $extension = Parser::parse("type Query { foo: String }");
+
+        $extended = Extender::extend($base, $extension);
+
+        $this->assertNotSame($base, $extended);
+        $this->assertSame("type Query {
+  foo: String
+}
+", Printer::doPrint($extended));
+    }
+
+    public function testDuplicateType(): void {
+        $this->expectExceptionMessage("Duplicate type definition 'Query'");
+        $this->expectException(RuntimeException::class);
+
+        $base = Parser::parse("type Query { foo: String }");
+        $extension = Parser::parse("type Query { foo: String }");
+
+        Extender::extend($base, $extension);
+    }
+
+    public function testUnusedExtension(): void {
+        $this->expectExceptionMessage("GraphQLASTExtender\Extender::extend: Missing base-types for type-extensions to 'Query'");
+        $this->expectException(RuntimeException::class);
+
+        $base = new DocumentNode([
+            "definitions" => new NodeList([]),
+        ]);
+        $extension = Parser::parse("extend type Query { foo: String }", [ "noLocation" => true ]);
+
+        Extender::extend($base, $extension);
+    }
+
+    public function testObjectFieldCollision(): void {
+        $base = Parser::parse("type Query { foo: Int }", [ "noLocation" => true ]);
+        $extension = Parser::parse("extend type Query { foo: String }", [ "noLocation" => true ]);
+
+        $extended = Extender::extend($base, $extension);
+
+        $this->assertNotSame($base, $extended);
+        // This will fail validation:
+        $this->assertSame("type Query {
+  foo: Int
+  foo: String
+}
+", Printer::doPrint($extended));
     }
 }
